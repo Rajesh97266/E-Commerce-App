c48b238380d2d901c14058989bcd683d2513b188
Rajesh97266 <rajesh97266@gmail.com>
2025-02-03 15:59:41 +0530

    common-form-component  created

---
+++ client/src/config/index.js
@@ -0,0 +1,23 @@
+export const registerFormControls = [
+  {
+    name: "userName",
+    type: "text",
+    placeholder: "Enter your user name",
+    label: "User Name",
+    componentType: "input",
+  },
+  {
+    name: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    name: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+];
