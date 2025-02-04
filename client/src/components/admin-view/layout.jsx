e1925c7f5ad1979ed544c3a6a6f0e0cc0d49f02b
Rajesh97266 <rajesh97266@gmail.com>
2025-02-04 15:29:23 +0530

    side-bar added

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./sidebar";
 import AdminHeader from "./header";
+import { useState } from "react";
+
 
 const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className="flex min-h-screen w-full">
       {/* Admin Sidebar  */}
-      <AdminSidebar />
+      <AdminSidebar open={openSidebar} setOpen={setOpenSidebar}/>
       <div className="flex flex-1 flex-col">
         {/* Admin Header */}
-        <AdminHeader />
+        <AdminHeader open={openSidebar} setOpen={setOpenSidebar}/>
         <main className="flex flex-1 bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
